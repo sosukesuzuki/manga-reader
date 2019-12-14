7472c3cd2b568b3b708da695cdf0eaeb79ff908e
Sosuke Suzuki <aosukeke@gmail.com>
2019-12-14 13:11:20 +0900

    Implement TopNav Home Button

--- src/components/template/TopNav.tsx
+++ src/components/template/TopNav.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
 import { Navbar, Button, Alignment } from '@blueprintjs/core';
 import { MAX_WIDTH } from '../../lib/constants';
+import { useNavigation } from 'react-navi';
 
 const CenteredGroup = styled(Navbar.Group)`
     width: ${MAX_WIDTH};
@@ -9,12 +10,21 @@ const CenteredGroup = styled(Navbar.Group)`
 `;
 
 const TopNav: React.FC = () => {
+    const navigation = useNavigation();
+    const handleClickHomeButton = useCallback(() => {
+        navigation.navigate('/');
+    }, [navigation]);
     return (
         <Navbar fixedToTop>
             <CenteredGroup align={Alignment.CENTER}>
                 <Navbar.Heading>漫画リーダー</Navbar.Heading>
                 <Navbar.Divider />
-                <Button className="bp3-minimal" icon="home" text="Home" />
+                <Button
+                    className="bp3-minimal"
+                    icon="home"
+                    text="Home"
+                    onClick={handleClickHomeButton}
+                />
             </CenteredGroup>
         </Navbar>
     );
