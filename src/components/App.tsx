20b8ecef2cb769e9fdde208cdbdde0737ff738ea
Sosuke Suzuki <aosukeke@gmail.com>
2019-12-14 11:51:18 +0900

    Show series

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,13 @@ import React, { Suspense } from 'react';
 import { routes } from '../lib/routes';
 import { Router, View } from 'react-navi';
 import Layout from '../components/Layout';
+import Loading from './molecules/Loading';
 
 const App: React.FC = () => {
     return (
         <Router routes={routes}>
             <Layout>
-                <Suspense fallback={null}>
+                <Suspense fallback={<Loading />}>
                     <View />
                 </Suspense>
             </Layout>
--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useCurrentRoute } from 'react-navi';
+import { Series } from '../../lib/types';
 
 const Top: React.FC = () => {
+    const route = useCurrentRoute();
+    const serieses: Series[] = route.data.data;
     return (
         <main>
             <p>ようこそ、漫画リーダーへ</p>
+            {serieses.map(series => {
+                return (
+                    <div key={series.seriesId}>
+                        <p>{series.title}</p>
+                        <img src={series.seriesImage} width="200" />
+                        <p>{series.description}</p>
+                    </div>
+                );
+            })}
         </main>
     );
 };
