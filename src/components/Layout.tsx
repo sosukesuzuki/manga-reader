9b36986f0c8729d6ad6c069297f56d8569e49c98
Sosuke Suzuki <aosukeke@gmail.com>
2019-12-14 13:46:39 +0900

    Apply max-width

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,10 +3,13 @@ import styled from 'styled-components';
 import BusyIndicator from 'react-busy-indicator';
 import TopNav from './template/TopNav';
 import { useLoadingRoute } from 'react-navi';
+import { MAX_WIDTH } from '../lib/constants';
 
 const MainContainer = styled.div`
     padding-top: 20px;
+    margin: 0 auto;
     margin-top: 50px;
+    max-width: ${MAX_WIDTH};
 `;
 
 const Layout: React.FC = ({ children }) => {
