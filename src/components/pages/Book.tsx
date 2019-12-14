9838ba779ecfb6ace84545484531dfbea517aa96
Sosuke Suzuki <aosukeke@gmail.com>
2019-12-14 15:56:13 +0900

    View bg-c

--- src/components/pages/Book.tsx
+++ src/components/pages/Book.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Book } from '../../lib/types';
 import Viewer from '../organisms/BookPage/Viewer';
+
+const Container = styled.div`
+    position: absolute;
+    top: 50px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: gray;
+    z-index: 0;
+`;
 
 interface Props {
     book: Book;
 }
 
 const Book: React.FC<Props> = ({ book }) => {
-    return <Viewer book={book} />;
+    return (
+        <Container>
+            <Viewer book={book} />;
+        </Container>
+    );
 };
 
 export default Book;
