f7c1725d8c1ef0279054ca04a6aa97340a86c707
Sosuke Suzuki <aosukeke@gmail.com>
2019-12-14 14:07:27 +0900

    Show title and desc

--- src/components/pages/Series.tsx
+++ src/components/pages/Series.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Series } from '../../lib/types';
 import SeriesImage from '../organisms/SeriesPage/SeriesImage';
+import { MAX_WIDTH } from '../../lib/constants';
+
+const Body = styled.div`
+    max-width: ${MAX_WIDTH};
+    margin: 0 auto;
+    padding: 20px;
+`;
 
 interface Props {
     series: Series;
 }
 
 const Series: React.FC<Props> = ({ series }) => {
-    const { seriesImage } = series;
+    const { seriesImage, title, description } = series;
     return (
         <main>
             <SeriesImage image={seriesImage} />
+            <Body>
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </Body>
         </main>
     );
 };
