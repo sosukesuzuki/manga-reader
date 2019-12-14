7f1dce9e6f00f5ef62356abad7af6253dbd64f6e
Sosuke Suzuki <aosukeke@gmail.com>
2019-12-14 11:20:38 +0900

    Add components dir

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
-
-function App() {
-    return <h1>漫画リーダー</h1>;
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('.root'));
