a6c070ad8a2f4d0c452512f525b2fc1fb2d4c3ca
Sosuke Suzuki <aosukeke@gmail.com>
2019-12-14 11:00:06 +0900

    Configure webpack

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+
+function App() {
+    return <h1>漫画リーダー</h1>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.root'));
