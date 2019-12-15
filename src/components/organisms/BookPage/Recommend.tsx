609ec5dda0edbf0c4671a792108b6aa93d00f3d4
Sosuke Suzuki <aosukeke@gmail.com>
2019-12-15 11:16:13 +0900

    Set absolute

--- src/components/organisms/BookPage/Recommend.tsx
+++ src/components/organisms/BookPage/Recommend.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Card } from '@blueprintjs/core';
+
+const AbsolutedCard = styled(Card)`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    max-width: 500px;
+    height: 300px;
+    @media screen and (max-width: 679px) {
+        margin: auto;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+    @media screen and (min-width: 680px) {
+        margin: auto;
+    }
+`;
 
 const Recommend: React.FC = () => {
     return (
-        <Card>
+        <AbsolutedCard>
             <h2>読み終わりました！</h2>
-        </Card>
+        </AbsolutedCard>
     );
 };
 
